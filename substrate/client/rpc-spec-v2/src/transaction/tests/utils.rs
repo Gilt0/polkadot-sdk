8fc6529cc20929359ff5f8c98f1e4934f82dd858
Alexandru Vasile <alexandru.vasile@parity.io>
2024-02-05 20:26:08 +0200

    tx/tests: Add wrapper for the transaction pool `submit_and_watch`
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/client/rpc-spec-v2/src/transaction/tests/utils.rs
+++ substrate/client/rpc-spec-v2/src/transaction/tests/utils.rs
@@ -22,13 +22,23 @@ use crate::{
 		TransactionBroadcast as RpcTransactionBroadcast, TransactionBroadcastMiddleware, *,
 	},
 };
+use codec::Encode;
 use futures::Future;
 use jsonrpsee::RpcModule;
 use sc_transaction_pool::{BasicPool, Options};
-use sc_transaction_pool_api::error::Error as PoolError;
+use sc_transaction_pool_api::{
+	error::Error as PoolError, ImportNotificationStream, PoolFuture, PoolStatus, ReadyTransactions,
+	TransactionFor, TransactionPool, TransactionSource, TransactionStatusStreamFor, TxHash,
+};
+
+use crate::hex_string;
+use futures::FutureExt;
+use futures::StreamExt;
+
 use sp_core::{testing::TaskExecutor, H256};
 use sp_runtime::traits::Block as BlockT;
-use std::{pin::Pin, sync::Arc};
+use sp_runtime::traits::NumberFor;
+use std::{collections::HashMap, pin::Pin, sync::Arc};
 use substrate_test_runtime_client::{prelude::*, Client};
 use substrate_test_runtime_transaction_pool::TestApi;
 use tokio::sync::mpsc;
@@ -172,5 +182,149 @@ impl TransactionBroadcastMiddleware<sc_transaction_pool_api::TransactionStatus<H
 		let _ = self
 			.sender
 			.send(MiddlewareEvent::Exit { id: operation_id.to_string(), is_aborted });
+	}
+}
+
+/// The type of the event that the middleware captures.
+#[derive(Debug, PartialEq)]
+pub enum MiddlewarePoolEvent {
+	TransactionStatus {
+		transaction: String,
+		status: sc_transaction_pool_api::TransactionStatus<
+			<Block as BlockT>::Hash,
+			<Block as BlockT>::Hash,
+		>,
+	},
+	PoolError {
+		transaction: String,
+		err: String,
+	},
+}
+
+/// Add a middleware to the transaction pool.
+///
+/// This wraps the `submit_and_watch` to gain access to the events.
+struct MiddlewarePool {
+	pool: Arc<BasicPool<TestApi, Block>>,
+	/// Send the middleware events to the test.
+	sender: mpsc::UnboundedSender<MiddlewarePoolEvent>,
+}
+
+impl MiddlewarePool {
+	/// Construct a new [`MiddlewarePool`].
+	fn new(
+		pool: Arc<BasicPool<TestApi, Block>>,
+	) -> (Self, mpsc::UnboundedReceiver<MiddlewarePoolEvent>) {
+		let (sender, recv) = mpsc::unbounded_channel();
+		(MiddlewarePool { pool, sender }, recv)
+	}
+}
+
+impl TransactionPool for MiddlewarePool {
+	type Block = <BasicPool<TestApi, Block> as TransactionPool>::Block;
+	type Hash = <BasicPool<TestApi, Block> as TransactionPool>::Hash;
+	type InPoolTransaction = <BasicPool<TestApi, Block> as TransactionPool>::InPoolTransaction;
+	type Error = <BasicPool<TestApi, Block> as TransactionPool>::Error;
+
+	fn submit_at(
+		&self,
+		at: <Self::Block as BlockT>::Hash,
+		source: TransactionSource,
+		xts: Vec<TransactionFor<Self>>,
+	) -> PoolFuture<Vec<Result<TxHash<Self>, Self::Error>>, Self::Error> {
+		self.pool.submit_at(at, source, xts)
+	}
+
+	fn submit_one(
+		&self,
+		at: <Self::Block as BlockT>::Hash,
+		source: TransactionSource,
+		xt: TransactionFor<Self>,
+	) -> PoolFuture<TxHash<Self>, Self::Error> {
+		self.pool.submit_one(at, source, xt)
+	}
+
+	fn submit_and_watch(
+		&self,
+		at: <Self::Block as BlockT>::Hash,
+		source: TransactionSource,
+		xt: TransactionFor<Self>,
+	) -> PoolFuture<Pin<Box<TransactionStatusStreamFor<Self>>>, Self::Error> {
+		let pool = self.pool.clone();
+		let sender = self.sender.clone();
+		let transaction = hex_string(&xt.encode());
+
+		async move {
+			let watcher = match pool.submit_and_watch(at, source, xt).await {
+				Ok(watcher) => watcher,
+				Err(err) => {
+					let _ = sender.send(MiddlewarePoolEvent::PoolError {
+						transaction: transaction.clone(),
+						err: err.to_string(),
+					});
+					return Err(err);
+				},
+			};
+
+			let watcher = watcher.map(move |status| {
+				let sender = sender.clone();
+				let transaction = transaction.clone();
+
+				let _ = sender.send(MiddlewarePoolEvent::TransactionStatus {
+					transaction,
+					status: status.clone(),
+				});
+
+				status
+			});
+
+			Ok(watcher.boxed())
+		}
+		.boxed()
+	}
+
+	fn remove_invalid(&self, hashes: &[TxHash<Self>]) -> Vec<Arc<Self::InPoolTransaction>> {
+		self.pool.remove_invalid(hashes)
+	}
+
+	fn status(&self) -> PoolStatus {
+		self.pool.status()
+	}
+
+	fn import_notification_stream(&self) -> ImportNotificationStream<TxHash<Self>> {
+		self.pool.import_notification_stream()
+	}
+
+	fn hash_of(&self, xt: &TransactionFor<Self>) -> TxHash<Self> {
+		self.pool.hash_of(xt)
+	}
+
+	fn on_broadcasted(&self, propagations: HashMap<TxHash<Self>, Vec<String>>) {
+		self.pool.on_broadcasted(propagations)
+	}
+
+	fn ready_transaction(&self, hash: &TxHash<Self>) -> Option<Arc<Self::InPoolTransaction>> {
+		self.pool.ready_transaction(hash)
+	}
+
+	fn ready_at(
+		&self,
+		at: NumberFor<Self::Block>,
+	) -> Pin<
+		Box<
+			dyn Future<
+					Output = Box<dyn ReadyTransactions<Item = Arc<Self::InPoolTransaction>> + Send>,
+				> + Send,
+		>,
+	> {
+		self.pool.ready_at(at)
+	}
+
+	fn ready(&self) -> Box<dyn ReadyTransactions<Item = Arc<Self::InPoolTransaction>> + Send> {
+		self.pool.ready()
+	}
+
+	fn futures(&self) -> Vec<Self::InPoolTransaction> {
+		self.pool.futures()
 	}
 }
